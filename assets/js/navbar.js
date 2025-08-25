40a42ca9f9942214567b6a7df02f73f1628045b5
jihenghu <hjh18305@gmail.com>
2025-08-25 15:41:56 -0400

    update blog

---
+++ assets/js/navbar.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector('.navbar');
+  let lastScrollTop = 0;
+  const threshold = 50; // 阈值，滚动超过 50px 才触发
+
+  window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    // 判断是否超过阈值
+    if (Math.abs(scrollTop - lastScrollTop) > threshold) {
+      if (scrollTop > lastScrollTop) {
+        // 向下滚动 -> 隐藏
+        navbar.classList.add('hide');
+      } else {
+        // 向上滚动 -> 显示
+        navbar.classList.remove('hide');
+      }
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // 避免负数
+    }
+  });
+});
